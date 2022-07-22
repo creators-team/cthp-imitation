905798c052b9a15fecc698d99c6ffb6718281181
reizt <reizt.dev@gmail.com>
2022-07-22 22:25:44 +0900

    Add comments & use for loop in typing animation instead of recursive call

--- scripts/index.js
+++ scripts/index.js
@@ -12,13 +12,14 @@ window.addEventListener('DOMContentLoaded', function(){
   });
   // パネルアニメーション
   document.addEventListener('mousemove', function(event){
-    const x = event.clientX;
-    const y = event.clientY;
+    const x = event.clientX; // ビューポートの左端からのマウスの距離
+    const y = event.clientY; // ビューポートの上端からのマウスの距離
     const screenWidth  = this.body.clientWidth;
     const screenHeight = this.documentElement.clientHeight;
-    const moveX = (1 - x * 2 / screenWidth) * 20
-    const moveY = (1 - y * 2 / screenHeight) * 20
+    const moveX = (1 - x * 2 / screenWidth) * 20;
+    const moveY = (1 - y * 2 / screenHeight) * 20;
     panelLinks.forEach(e=>{
+      // なぜかわからないけどrotateXにmoveYを、rotateYにmoveXを使うとマウスの方向を向く
       e.style.setProperty('--rotateX', moveY + 'deg');
       e.style.setProperty('--rotateY', -moveX + 'deg');
     });
@@ -31,39 +32,40 @@ window.addEventListener('DOMContentLoaded', function(){
     cursorHTML: '<span class="typing-cursor"></span>',
     staticHTML: ' クリエイター集団',
   }
-  let source = data.firstText;
-  let result = data.firstText;
-  let displayLength = data.firstText.length;
+  let source = data.firstText; // firstTextまたはsecondText
+  let result = data.firstText; // 表示中の文字
+  let displayLength = data.firstText.length; // 表示する文字数
   const changeText = text => {typingElem.innerHTML = text}
-  const tryChangeText = ()=>{
-    switch(displayLength){
-    case 0:
-      changeText(result + data.staticHTML);
-      setTimeout(()=>{
+  (async ()=>{
+    const sleep = (miliSecond) => {
+      return new Promise(resolve => setTimeout(resolve, miliSecond));
+    }
+    for(;;){
+      if(displayLength === 0){
+        // 何も表示しないとき
+        // 動的な文字を消して1000ms待ったら次へ
+        changeText(result + data.staticHTML);
+        await sleep(1000);
         displayLength++;
-        tryChangeText();
-      }, 1000);
-      break;
-    case source.length + 1:
-      if(source === data.firstText){
-        source = data.secondText;
-      }else{
-        source = data.firstText;
-      }
-      setTimeout(()=>{
+      }else if(displayLength === source.length + 1){
+        // 表示する文字数が最大値を超えたとき
+        // ソースを入れ替えて値をリセット、次に備える
+        if(source === data.firstText){
+          source = data.secondText;
+        }else{
+          source = data.firstText;
+        }
+        await sleep(2700);
         result = '';
         displayLength = 0;
-        tryChangeText();
-      }, 2700);
-      break;
-    default:
-      result = source.slice(0, displayLength);
-      changeText(result + data.cursorHTML + data.staticHTML);
-      setTimeout(()=>{
+      }else{
+        // 1 ~ source文字数 まで表示するとき
+        // 文字を変更して150ms後に次へ
+        result = source.slice(0, displayLength);
+        changeText(result + data.cursorHTML + data.staticHTML);
+        await sleep(150);
         displayLength++;
-        tryChangeText();
-      }, 150);
+      }
     }
-  }
-  tryChangeText();
+  })()
 });
